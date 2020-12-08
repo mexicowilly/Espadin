ace06a66d73f73d67e9ef7ee49d02f44c06b9626
Will Mason <willchido@gmail.com>
2020-12-08 13:28:14 -0800

    Added progress tests for files create

--- test/files_create_test.cpp
+++ test/files_create_test.cpp
@@ -53,7 +53,7 @@ TEST_F(files_create, small)
     std::filesystem::path f = std::filesystem::temp_directory_path() / "files_create_small.txt";
     std::ofstream stream(f);
     for (int i = 0; i < 2 * 1024 * 1024; i++)
-        stream << 'a';
+        stream << static_cast<char>((i % 32) + 32);
     stream.close();
     espadin::file md;
     md.parents({parent_})
@@ -66,12 +66,32 @@ TEST_F(files_create, small)
     EXPECT_TRUE(reply->mime_type());
 }
 
+TEST_F(files_create, small_with_progress)
+{
+    std::filesystem::path f = std::filesystem::temp_directory_path() / "files_create_small_with_progress.txt";
+    std::ofstream stream(f);
+    for (int i = 0; i < 2 * 1024 * 1024; i++)
+        stream << static_cast<char>((i % 32) + 32);
+    stream.close();
+    espadin::file md;
+    md.parents({parent_})
+      .name(f.filename().string());
+    auto crt = files_->create(md, f);
+    crt->progress_callback([] (double pct) { std::cout << pct << '%' << std::endl; });
+    auto reply = crt->run();
+    std::filesystem::remove(f);
+    EXPECT_TRUE(reply->kind());
+    EXPECT_TRUE(reply->id());
+    EXPECT_TRUE(reply->name());
+    EXPECT_TRUE(reply->mime_type());
+}
+
 TEST_F(files_create, large)
 {
     std::filesystem::path f = std::filesystem::temp_directory_path() / "files_create_large.txt";
     std::ofstream stream(f);
     for (int i = 0; i < 6 * 1024 * 1024; i++)
-        stream << 'a';
+        stream << static_cast<char>((i % 32) + 32);
     stream.close();
     espadin::file md;
     md.parents({parent_})
@@ -83,20 +103,23 @@ TEST_F(files_create, large)
     EXPECT_TRUE(reply->name());
     EXPECT_TRUE(reply->mime_type());
 }
-//TEST_F(files_create, char_data)
-//{
-//    espadin::file f;
-//    f.parents({parent_})
-//     .name("my stuff.json")
-//     .mime_type("application/json");
-//    std::vector<std::byte> data;
-//    std::string str("{ \"hello\": \"goodbye\" }");
-//    for (auto c : str)
-//        data.push_back(static_cast<std::byte>(c));
-//    auto create = files_->create(f, data);
-//    auto reply = create->run();
-//    EXPECT_TRUE(reply->kind());
-//    EXPECT_TRUE(reply->id());
-//    EXPECT_TRUE(reply->name());
-//    EXPECT_TRUE(reply->mime_type());
-//}
+
+TEST_F(files_create, large_with_progress)
+{
+    std::filesystem::path f = std::filesystem::temp_directory_path() / "files_create_large_with_progress.txt";
+    std::ofstream stream(f);
+    for (int i = 0; i < 6 * 1024 * 1024; i++)
+        stream << static_cast<char>((i % 32) + 32);
+    stream.close();
+    espadin::file md;
+    md.parents({parent_})
+      .name(f.filename().string());
+    auto crt = files_->create(md, f);
+    crt->progress_callback([] (double pct) { std::cout << pct << '%' << std::endl; });
+    auto reply = crt->run();
+    std::filesystem::remove(f);
+    EXPECT_TRUE(reply->kind());
+    EXPECT_TRUE(reply->id());
+    EXPECT_TRUE(reply->name());
+    EXPECT_TRUE(reply->mime_type());
+}
