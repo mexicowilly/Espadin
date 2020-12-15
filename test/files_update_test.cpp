c1638fa409d23f04fcf175398b1c20e6fca6ed71
Will Mason <willchido@gmail.com>
2020-12-14 20:37:51 -0800

    Finish files update

--- test/files_update_test.cpp
+++ test/files_update_test.cpp
@@ -49,6 +49,25 @@ protected:
 
 }
 
+TEST_F(files_update, contents)
+{
+    std::filesystem::path f(std::filesystem::temp_directory_path() / "files_update_contents.txt");
+    std::ofstream stream(f);
+    for (int i = 0; i < 2 * 1024 * 1024; i++)
+        stream << static_cast<char>((i % 32) + 32);
+    stream.close();
+    espadin::file metadata;
+    metadata.name(f.filename().string());
+    files_->update(to_update_, std::move(metadata), f)->run();
+    auto get = files_->get(to_update_);
+    auto reply = get->fields("name,size").run();
+    EXPECT_TRUE(reply->name());
+    EXPECT_EQ(f.filename().string(), *reply->name());
+    EXPECT_TRUE(reply->size());
+    EXPECT_EQ(std::filesystem::file_size(f), *reply->size());
+    std::filesystem::remove(f);
+}
+
 TEST_F(files_update, empty)
 {
     std::string new_name("are you serious?");
